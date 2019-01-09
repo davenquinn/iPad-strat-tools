a9d59919584f2ed286a6ae43a5b12f1c4aa7aa26
Daven Quinn <dev@davenquinn.com>
2019-01-09 00:44:22 -0600

    Updated CLI with split options

--- strat_tools/cli.py
+++ strat_tools/cli.py
@@ -9,8 +9,11 @@ from wand.image import Image as WandImage
 @click.command(name='stack-section')
 @click.argument('infile', type=click.Path(exists=True))
 @click.argument('outfile', type=click.Path())
-@click.option('--dpi', type=int, default=300)
-def cli(infile, outfile, dpi=300):
+@click.option('--dpi', type=int, default=300, help="Dots per inch for conversion")
+@click.option('--clip-left', type=float, default=0, help="Left crop, in inches")
+@click.option('--clip-right', type=float, default=0, help="Right crop, in inches")
+@click.option('--split-threshold', type=int, help="Threshold to split output image")
+def cli(infile, outfile, dpi=300, clip_left=0, clip_right=0, split_threshold=None):
 
     secho(infile, fg='green')
 
@@ -21,11 +24,6 @@ def cli(infile, outfile, dpi=300):
         n = len(img.sequence)
         secho('pages = '+str(n))
 
-        height = img.height
-        width = img.width
-
-        height_inches = height/dpi
-
         pixels_per_box = dpi/2.54
 
         # The working area (with grid) is 17 boxes tall by 13 boxes wide
@@ -33,10 +31,16 @@ def cli(infile, outfile, dpi=300):
         # section is 15 boxes high in the center of the image in our current
         # template
         working_height = (n_boxes[1]-2)*pixels_per_box
-        margin = (height-working_height)/2
+        margin = (img.height-working_height)/2
         total_height = round(working_height*n+2*margin)
 
-        surface = Image.new('RGBA', (img.width, total_height))
+        # Set right and left crop
+        left = round(clip_left*dpi)
+        right = round(clip_right*dpi)
+
+        width = img.width-left-right
+
+        surface = Image.new('RGBA', (width, total_height))
 
         for i, page in enumerate(img.sequence):
             bytes = BytesIO()
@@ -44,10 +48,19 @@ def cli(infile, outfile, dpi=300):
             with p.convert('png') as converted:
                 converted.save(bytes)
             img = Image.open(bytes)
-            cropped = img.crop((0, round(margin), width, round(margin+working_height)))
+
+            # Set top
+            top = round(margin)
+            if i == n-1: top = 0
+
+            # bottom
+            bottom = round(margin+working_height)
+            if i == 0: bottom = img.height
+
+            cropped = img.crop((left, top, img.width-right, bottom))
             # The most this will ever be off from desired height
             # is half a pixel.
-            insert_height = round(working_height*(n-1-i)+margin)
+            insert_height = round(working_height*(n-1-i)+top)
             ul = (0,insert_height)
 
             surface.paste(cropped, ul)
